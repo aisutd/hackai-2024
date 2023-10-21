08fa892e720440a6aeaf6b5a71c0332c708ba196
MF2025 <mlf200000@utdallas.edu>
2023-10-21 14:52:35 -0500

    Edits to landing section + changed favicon

--- pages/Landing.tsx
+++ pages/Landing.tsx
@@ -8,8 +8,6 @@ export default function Landing() {
     <>
       <div className="absolute py-8 w-[100vw]">
       <div className="absolute bg-[url('/landing.svg')] bg-no-repeat bg-contain bg-center h-[100vh] w-[100vw] -z-40"/>
-      <button className="z-10 flex w-275 h-66 justify-center items-center gap-10 flex-shrink-0 rounded-17.57 border-3.3 border-var-Red bg-var-Background shadow-md">
-      </button>
       <RegisterPulse className="absolute mt-[73vh] ml-[calc(50vw-8rem)] w-[16rem] h-[2rem] z-0"/>
       </div>
     </>
